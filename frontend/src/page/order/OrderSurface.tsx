fe8cd7554fd142b77185f90487c672db678954d6
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-12-23 23:15:51 -0300

    frontend: add html structure to `OrderSurface` page

--- frontend/src/page/order/OrderSurface.tsx
+++ frontend/src/page/order/OrderSurface.tsx
@@ -1,3 +1,42 @@
+import { Link, useLoaderData } from 'react-router-dom';
+import OrderDisplay, { Order } from '@/component/order/OrderDisplay';
+import { AppPath } from '@/config/router';
+import DeliveryEventDisplay, {
+	DeliveryEvent,
+} from '@/component/delivery_event/DeliveryEventDisplay';
+
 export default function OrderSurface() {
-	return <span>Order</span>;
+	// const { id } = useParams();
+	const order = useLoaderData() as Order | null;
+	const events: Array<DeliveryEvent> = [];
+
+	return order ? (
+		<main id="order-surface">
+			<div>
+				<h3>{'Pedido #' + order.id}</h3>
+			</div>
+
+			<OrderDisplay order={order} show_id={false} />
+			<button>Atualizar</button>
+
+			<section id="history">
+				<h4>Histórico</h4>
+
+				<div>
+					{events.map(event => (
+						<DeliveryEventDisplay event={event} />
+					))}
+				</div>
+			</section>
+		</main>
+	) : (
+		<main>
+			<div className="ContainerStyle.content_not_found">
+				<h1 className="TypographyStyle.heading3">Pedido não encontrado</h1>
+				<p className="TypographyStyle.body2">
+					Verifique sua URL ou volte para os <Link to={AppPath.Order}>pedidos</Link>.
+				</p>
+			</div>
+		</main>
+	);
 }
