06f8d5b14c169735cacba23b6097ef1e7629eca6
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-12-23 21:57:34 -0300

    frontend: add html sctructure to `SignIn` and `SignUp` page

--- frontend/src/page/auth/SignIn.tsx
+++ frontend/src/page/auth/SignIn.tsx
@@ -1,3 +1,20 @@
+import { AppPath } from '@/config/router';
+import { Link } from 'react-router-dom';
+
 export default function SignIn() {
-	return <span>SignIn</span>;
+	return (
+		<main id="signin">
+			<div>
+				<p>Ainda não possui uma conta?</p>
+				<Link to={AppPath.SignUp}>Criar conta</Link>
+			</div>
+
+			<form id="signin-form">
+				<p>Sign-In</p>
+				{/* <TextField label="E-mail" /> */}
+				{/* <TextField label="Senha" /> */}
+				<button type="submit">Entrar</button>
+			</form>
+		</main>
+	);
 }
--- frontend/src/page/auth/SignUp.tsx
+++ frontend/src/page/auth/SignUp.tsx
@@ -1,3 +1,21 @@
+import { Link } from 'react-router-dom';
+import { AppPath } from '@/config/router';
+
 export default function SignUp() {
-	return <span>SignUp</span>;
+	return (
+		<main id="signup">
+			<div>
+				<p>Já possui uma conta?</p>
+				<Link to={AppPath.SignIn}>Entrar</Link>
+			</div>
+
+			<form id="signup-form">
+				<p>Sign-Up</p>
+				{/* <TextField label="Nome" /> */}
+				{/* <TextField label="E-mail" /> */}
+				{/* <TextField label="Senha" /> */}
+				<button type="submit">Criar</button>
+			</form>
+		</main>
+	);
 }
