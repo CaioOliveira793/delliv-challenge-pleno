f2c261ed370c8cbb57d32df95365c73880c6898d
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-12-24 18:18:31 -0300

    frontend: add styles to `TopNavbar` component

---
+++ frontend/src/component/app/TopNavbar/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import { AppPath } from '@/config/router';
+import { buttonClass } from '@/component/form/ButtonHelper';
+import TopNavbarStyle from '@/component/navigation/TopNavbar/TopNavbar.module.css';
+
+export default function TopNavbar() {
+	return (
+		<nav className={TopNavbarStyle.container}>
+			<ul className={TopNavbarStyle.nav_item_list}>
+				<li>
+					<Link to={AppPath.SignIn} className={buttonClass({ variant: 'outlined', size: 'large' })}>
+						Entrar
+					</Link>
+				</li>
+				<li>
+					<Link
+						to={AppPath.SignUp}
+						className={buttonClass({ variant: 'contained', size: 'large' })}
+					>
+						Criar conta
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
