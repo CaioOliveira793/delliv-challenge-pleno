1eeb44a01599a08342e46e1bae5dd3b528426269
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-12-25 13:31:47 -0300

    frontend: add styles to `OrderDisplay` component

---
+++ frontend/src/component/order/OrderDisplay/index.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { AppPath } from '@/config/router';
+import TypographyStyle from '@/style/typography.module.css';
+import OrderDisplayStyle from './OrderDisplay.module.css';
+
+export interface Order {
+	id: string;
+	created: Date;
+	updated: Date;
+	status: string;
+	customer_name: string;
+	delivery_address: string;
+}
+
+interface OrderDisplayProps {
+	order: Order;
+	show_id?: boolean;
+}
+
+export default function OrderDisplay({ order, show_id = true }: OrderDisplayProps) {
+	return (
+		<div className={OrderDisplayStyle.container}>
+			{show_id ? (
+				<Link to={AppPath.Order + '/' + order.id} className={OrderDisplayStyle.order_link}>
+					{'#' + order.id}
+				</Link>
+			) : null}
+			<p>
+				<span className={TypographyStyle.attr_name}>Status: </span>
+				<span className={TypographyStyle.attr_value}>{order.status}</span>
+			</p>
+			<p>
+				<span className={TypographyStyle.attr_name}>Cliente: </span>
+				<span className={TypographyStyle.attr_value}>{order.customer_name}</span>
+			</p>
+			<p>
+				<span className={TypographyStyle.attr_name}>Endereço de entrega: </span>
+				<span className={TypographyStyle.attr_value}>{order.delivery_address}</span>
+			</p>
+		</div>
+	);
+}
