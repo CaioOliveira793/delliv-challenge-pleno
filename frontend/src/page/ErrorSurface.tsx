6d51245e99100edc5a3974105b7d3ddb359af61d
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-12-25 03:05:26 -0300

    frontend: add styles to `ErrorSurface` page

--- frontend/src/page/ErrorSurface.tsx
+++ frontend/src/page/ErrorSurface.tsx
@@ -1,17 +1,27 @@
-import { useRouteError, ErrorResponse } from 'react-router-dom';
+import { useRouteError, ErrorResponse, Link } from 'react-router-dom';
+import { AppPath } from '@/config/router';
+import ContainerStyle from '@/style/util/container.module.css';
+import TypographyStyle from '@/style/typography.module.css';
+import ErrorSurfaceStyle from '@/style/page/ErrorSurface.module.css';
+import { classes } from '@/style/helper';
 
 export default function ErrorSurface() {
 	const error = useRouteError() as ErrorResponse & Error;
 	console.error(error);
 
 	return (
-		<main id="error-page">
-			<h1>Oops!</h1>
-			<p>Desculpe, um erro inesperado ocorreu.</p>
-			<p>
+		<main
+			id="error-page"
+			className={classes(ErrorSurfaceStyle.container, ContainerStyle.main_content)}
+		>
+			<h1 className={TypographyStyle.heading1}>Oops!</h1>
+			<p className={TypographyStyle.body2}>Desculpe, um erro inesperado ocorreu.</p>
+			<p className={classes(ErrorSurfaceStyle.error_paragraph, TypographyStyle.caption)}>
 				<i>{error?.statusText || error.message}</i>
 			</p>
-			{/* TODO: go back to the main page */}
+			<p className={TypographyStyle.body2}>
+				Voltar para <Link to={AppPath.Root}>página inicial</Link>
+			</p>
 		</main>
 	);
 }
