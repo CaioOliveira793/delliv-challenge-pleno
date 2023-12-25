22bff1054819737eb733df0e37be560c7def7d01
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-12-25 02:51:21 -0300

    frontend: add styles to `NotFound` page

--- frontend/src/page/NotFound.tsx
+++ frontend/src/page/NotFound.tsx
@@ -1,18 +1,22 @@
-import { AppPath } from '@/config/router';
 import { Link } from 'react-router-dom';
+import { AppPath } from '@/config/router';
+import { classes } from '@/style/helper';
+import TypographyStyle from '@/style/typography.module.css';
+import ContainerStyle from '@/style/util/container.module.css';
+import NotFoundStyle from '@/style/page/NotFound.module.css';
 
 export default function NotFound() {
 	const isUserAuthenticated = false;
 	return (
-		<main id="not-found-page" className="ContainerStyle.content_not_found">
-			<h1 className="TypographyStyle.heading3">Esta página não existe</h1>
+		<main id="not-found" className={classes(ContainerStyle.main_content, NotFoundStyle.container)}>
+			<h1 className={TypographyStyle.heading3}>Esta página não existe</h1>
 			{isUserAuthenticated ? (
-				<p>
-					Verifique sua URL ou volte para a<Link to={AppPath.Order}>página inicial</Link>.
+				<p className={TypographyStyle.body2}>
+					Verifique sua URL ou volte para a <Link to={AppPath.Order}>página inicial</Link>.
 				</p>
 			) : (
-				<p>
-					Verifique sua URL ou volte para a<Link to={AppPath.SignIn}>página de sign-in</Link>.
+				<p className={TypographyStyle.body2}>
+					Verifique sua URL ou volte para a <Link to={AppPath.SignIn}>página de sign-in</Link>.
 				</p>
 			)}
 		</main>
