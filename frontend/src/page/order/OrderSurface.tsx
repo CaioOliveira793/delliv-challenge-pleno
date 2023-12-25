14bdf6afd66c5e88e26dd16cc6484e179a7c29a6
CaioOliveira793 <caio.vsoliveira23@gmail.com>
2023-12-25 14:49:31 -0300

    frontend: add styles to OrderSurface page

--- frontend/src/page/order/OrderSurface.tsx
+++ frontend/src/page/order/OrderSurface.tsx
@@ -1,39 +1,73 @@
 import { Link, useLoaderData } from 'react-router-dom';
-import OrderDisplay, { Order } from '@/component/order/OrderDisplay';
 import { AppPath } from '@/config/router';
 import DeliveryEventDisplay, {
 	DeliveryEvent,
 } from '@/component/delivery_event/DeliveryEventDisplay';
+import OrderDisplay, { Order } from '@/component/order/OrderDisplay';
+import TopBar from '@/component/app/TopBar';
+import Button from '@/component/form/Button';
+import { classes } from '@/style/helper';
+import ContainerStyle from '@/style/util/container.module.css';
+import TypographyStyle from '@/style/typography.module.css';
+import OrderSurfaceStyle from '@/style/page/order/OrderSurface.module.css';
+
+const FAKE_ORDER: Order = {
+	id: '1',
+	created: new Date(),
+	updated: new Date('2023-12-29T16:56:34.470Z'),
+	status: 'Entregue',
+	customer_name: 'Yasmin Alana',
+	delivery_address: 'Rua Artur de Barros, n 829, Aclimação, Uberlândia, MG',
+};
+
+const FAKE_DELIVERY_EVENT_1: DeliveryEvent = {
+	id: '2',
+	creator_id: '1',
+	order_id: FAKE_ORDER.id,
+	created: new Date(),
+	status: 'Entregue',
+	message: null,
+};
+const FAKE_DELIVERY_EVENT_2: DeliveryEvent = {
+	id: '1',
+	creator_id: '1',
+	order_id: FAKE_ORDER.id,
+	created: new Date(),
+	status: 'Postado',
+	message: 'Objeto postado pelo remetente',
+};
 
 export default function OrderSurface() {
-	// const { id } = useParams();
-	const order = useLoaderData() as Order | null;
-	const events: Array<DeliveryEvent> = [];
+	const order = (useLoaderData() as Order | null) ?? FAKE_ORDER;
+	const events: Array<DeliveryEvent> = [FAKE_DELIVERY_EVENT_1, FAKE_DELIVERY_EVENT_2];
 
 	return order ? (
-		<main id="order-surface">
-			<div>
-				<h3>{'Pedido #' + order.id}</h3>
-			</div>
-
+		<main
+			id="order-surface"
+			className={classes(ContainerStyle.main_content, OrderSurfaceStyle.container)}
+		>
+			<TopBar />
+			<h3 className={TypographyStyle.heading3}>{'Pedido #' + order.id}</h3>
 			<OrderDisplay order={order} show_id={false} />
-			<button>Atualizar</button>
+			<Button color="info" className={OrderSurfaceStyle.update_button}>
+				Atualizar
+			</Button>
 
-			<section id="history">
-				<h4>Histórico</h4>
+			<section id="history" className={OrderSurfaceStyle.event_history_section}>
+				<h4 className={TypographyStyle.heading4}>Histórico</h4>
 
-				<div>
+				<div className={OrderSurfaceStyle.event_list}>
 					{events.map(event => (
-						<DeliveryEventDisplay event={event} />
+						<DeliveryEventDisplay key={event.id} event={event} />
 					))}
 				</div>
 			</section>
 		</main>
 	) : (
-		<main>
-			<div className="ContainerStyle.content_not_found">
-				<h1 className="TypographyStyle.heading3">Pedido não encontrado</h1>
-				<p className="TypographyStyle.body2">
+		<main className={ContainerStyle.main_content}>
+			<div className={ContainerStyle.content_not_found}>
+				<h1 className={TypographyStyle.heading3}>Pedido não encontrado</h1>
+				<p className={TypographyStyle.body2}>
 					Verifique sua URL ou volte para os <Link to={AppPath.Order}>pedidos</Link>.
 				</p>
 			</div>
--- frontend/src/page/order/SearchOrder.tsx
+++ frontend/src/page/order/SearchOrder.tsx
@@ -6,8 +6,17 @@ import TypographyStyle from '@/style/typography.module.css';
 import SearchOrderStyle from '@/style/page/order/SearchOrder.module.css';
 import { classes } from '@/style/helper';
 
+const FAKE_ORDER: Order = {
+	id: '1',
+	created: new Date(),
+	updated: new Date(),
+	status: 'Entregue',
+	customer_name: 'Yasmin Alana',
+	delivery_address: 'Rua Artur de Barros, n 829, Aclimação, Uberlândia, MG',
+};
+
 export default function SearchOrder() {
-	const orders: Array<Order> = [];
+	const orders: Array<Order> = [FAKE_ORDER];
 
 	return (
 		<main
@@ -15,16 +24,11 @@ export default function SearchOrder() {
 			className={classes(ContainerStyle.main_content, SearchOrderStyle.container)}
 		>
 			<TopBar />
-
-			<div>
-				<h3 className={TypographyStyle.heading3}>Pedidos</h3>
-			</div>
-
+			<h3 className={TypographyStyle.heading3}>Pedidos</h3>
 			<OrderSearchForm />
-
-			<div>
+			<div className={SearchOrderStyle.order_list}>
 				{orders.map(order => (
-					<OrderDisplay order={order} />
+					<OrderDisplay key={order.id} order={order} />
 				))}
 			</div>
 		</main>
